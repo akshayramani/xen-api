938862c76d06128c8891f598ef9b501a6ff7f48a
Rob Hoes <rob.hoes@citrix.com>
2011-01-04 15:50:39 +0000

    improve v6testd error reporting

--- fakev6.ml
+++ fakev6.ml
@@ -19,24 +19,27 @@ open Edition
 let supported_editions = [Free; Advanced; Enterprise; Enterprise_xd; Platinum]
 
 let apply_edition edition additional_params =
-	let edition' = Edition.of_string edition in
-	if List.mem edition' supported_editions then begin
-		let name = Edition.to_marketing_name edition' in
-		(* define "never" as 01-01-2030 *)
-		let start_of_epoch = Unix.gmtime 0. in
-		let never, _ = Unix.mktime {start_of_epoch with Unix.tm_year = 130} in
-		let license =
-			{
-				(License.default ()) with
-				License.sku = edition;
-				License.sku_marketing_name = name;
-				License.expiry = never;
-			}
-		in
-		edition, Edition.to_features edition', License.to_assoc_list license
-	end else
-		failwith "unknown edition"
-		
+	try
+		let edition' = Edition.of_string edition in
+		if List.mem edition' supported_editions then begin
+			let name = Edition.to_marketing_name edition' in
+			(* define "never" as 01-01-2030 *)
+			let start_of_epoch = Unix.gmtime 0. in
+			let never, _ = Unix.mktime {start_of_epoch with Unix.tm_year = 130} in
+			let license =
+				{
+					(License.default ()) with
+					License.sku = edition;
+					License.sku_marketing_name = name;
+					License.expiry = never;
+				}
+			in
+			edition, Edition.to_features edition', License.to_assoc_list license
+		end else
+			raise (Edition.Undefined_edition edition)
+	with Edition.Undefined_edition e ->
+		raise (V6errors.Error (V6errors.invalid_edition, [edition]))
+
 let get_editions () =
 	List.map (fun e -> Edition.to_string e, Edition.to_marketing_name e,
 		Edition.to_short_string e, Edition.to_int e) supported_editions
