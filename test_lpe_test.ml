9693db1d04b86913fce5baae90fdaff9a96a2a0c
Mike McClurg <mike.mcclurg@citrix.com>
2013-02-21 07:41:38 +0000

    CP-4276: start writing unit tests
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

---
+++ test_lpe_test.ml
@@ -0,0 +1,9 @@
+(* TODO Write unit tests for Lpe_test module *)
+
+let test_read_mock_license () =
+	(* write /tmp/mock.lic file *)
+	(* call read_mock_license () *)
+	(* test output *)
+	(* delete /tmp/mock.lic file *)
+	let lic = Lpe_test.read_mock_license () in
+	ignore lic
