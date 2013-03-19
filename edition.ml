akshayramani/xen-api

commit e269d51c6cd17460aaf9c8acc9313794f5e7d698

CP-4273: prepare for Hotfix_apply and RPU features
Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

edition.ml
@@ -63,7 +63,11 @@ let free_features =
 let additional_free_features = Lab :: Stage :: StorageLink_site_recovery :: StorageLink ::
 	Web_self_service_manager :: Web_self_service :: Vswitch_controller :: []
 
-let to_features _ = free_features
+let paid_features = [] (* will be [ Hotfix_apply ; RPU ] *)
+
+let to_features = function
+	| Free -> free_features
+	| _ -> paid_features
 
 let to_additional_features _ = additional_free_features
 

commit b48e1357e5f43768c9cabb3acd06a62e0f89f3b1

CP-4273/CA-100716: fix paid_features=[] bug
Introduced this bug in e269d51c, where we accidentally set paid features to be
empty, instead of the union of paid and free features.

Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

edition.ml
@@ -67,7 +67,7 @@ let paid_features = [] (* will be [ Hotfix_apply ; RPU ] *)
 
 let to_features = function
 	| Free -> free_features
-	| _ -> paid_features
+	| _ -> paid_features @ free_features
 
 let to_additional_features _ = additional_free_features
 